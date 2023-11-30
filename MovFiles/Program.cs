4eb40af3ddb4b3d5c9d29870f69fe88743a0a2d6
Dominik <dominik.kolodziejczak95@o2.pl>
2023-11-30 09:30:25 +0100

    Add project files.

---
+++ MovFiles/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the path of the directory:");
+        string directoryPath = Console.ReadLine();
+
+        if (Directory.Exists(directoryPath))
+        {
+            Console.WriteLine("\nList of folders and subfolders:");
+
+            // Call the recursive function
+            DisplayFolders(directoryPath, "");
+
+            Console.WriteLine("\nFinished processing.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid directory path or directory does not exist.");
+        }
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    
+    }
+
+    static void DisplayFolders(string folderPath, string indent)
+    {
+        string[] folders = Directory.GetDirectories(folderPath);
+
+        foreach (var folder in folders)
+        {
+            Console.WriteLine($"{indent} - {Path.GetFileName(folder)}");
+
+            // Check if .mov files exist in the current folder
+            string[] movFiles = Directory.GetFiles(folder, "*.mov");
+            bool isCreated = false;
+
+            if (movFiles.Length > 0)
+            {
+                // Create "MOV" folder if it doesn't exist
+                string newPath = folder + "\\" + Path.GetFileName(folder);
+
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                    isCreated = true;
+                    Console.WriteLine($"Created 'MOV' folder in: {Path.GetFileName(newPath)}");
+                }
+
+                foreach (var movFile in movFiles)
+                {
+                    string movFileName = Path.GetFileName(movFile);
+                    string destinationPath = newPath + "\\" + movFileName;
+                    File.Move(movFile, destinationPath);
+                    Console.WriteLine($"Moved {Path.GetFileName(movFile)} to 'MOV' folder");
+                }
+            }
+
+            if (isCreated)
+            {
+                continue;
+            }
+
+            // Recursively call the function for subfolders
+            DisplayFolders(folder, indent + "  ");
+        }
+
+    }
+}
